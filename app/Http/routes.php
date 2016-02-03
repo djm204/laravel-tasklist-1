1f42d7aaa1b1b510d2f4ec7d5fe57e5e1079897e
David Mendez <desiratech@gmail.com>
2016-02-03 17:04:58 -0600

    validation for task name

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,8 +40,19 @@ Route::group(['middleware' => ['web']], function () {
     
     //Add task
     Route::post('/task', function (Request $request){
-        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+         ]);
+         
+         if ($validator -> fails()){
+             return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+         }
+         
+         //create the task
     });
+    
     
     //Delete a task
     Route::delete('/task/{task}', function (Task $task){
