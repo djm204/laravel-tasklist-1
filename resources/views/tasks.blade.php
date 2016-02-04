072bc4af3b07201ff73aabd8f258184aa6673043
David Mendez <desiratech@gmail.com>
2016-02-03 20:39:48 -0600

    added in the bootstrap boilerplate

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,84 +4,91 @@
 
 @section('content')
 
-    <!-- Bootstrap Boilerplate... -->
+    <div class="container">
+		<div class="col-sm-offset-2 col-sm-8">
+			
+				<div class="panel-heading">
+					New Task
+				</div>
 
-    <div class="panel-body">
-        <!-- Display Validation Errors -->
-        @include('common.errors')
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
 
-        <!-- New Task Form -->
-        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-            {!! csrf_field() !!}
+                    <!-- New Task Form -->
+                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                        {!! csrf_field() !!}
 
-            <!-- Task Name -->
-            <div class="form-group">
-                <label for="task" class="col-sm-3 control-label">Task</label>
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task" class="col-sm-3 control-label">Task</label>
 
-                <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i> Add Task
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </div>
+                    
+                @if (count($tasks) > 0)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Current Tasks
+                        </div>
 
-            <!-- Add Task Button -->
-            <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Task
-                    </button>
-                </div>
-            </div>
-        </form>
-    </div>
+                        <div class="panel-body">
+                            <table class="table table-striped task-table">
 
-    @if (count($tasks) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Current Tasks
-            </div>
+                                <!-- Table Headings -->
+                                <thead>
+                                    <th>Task</th>
+                                    <th>&nbsp;</th>
+                                </thead>
 
-            <div class="panel-body">
-                <table class="table table-striped task-table">
+                                <!-- Table Body -->
+                                <tbody>
+                                    @foreach ($tasks as $task)
+                                        <tr>
+                                            <!-- Task Name -->
+                                            <td class="table-text">
+                                                <div>{{ $task->name }}</div>
+                                            </td>
 
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>Task</th>
-                        <th>&nbsp;</th>
-                    </thead>
+                                            <td>
+                                                <tr>
+                                                    <!-- Task Name -->
+                                                    <td class="table-text">
+                                                        <div>{{ $task->name }}</div>
+                                                    </td>
 
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($tasks as $task)
-                            <tr>
-                                <!-- Task Name -->
-                                <td class="table-text">
-                                    <div>{{ $task->name }}</div>
-                                </td>
+                                                    <!-- Delete Button -->
+                                                    <td>
+                                                        <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                                            {!! csrf_field() !!}
+                                                            {!! method_field('DELETE') !!}
 
-                                <td>
-                                    <tr>
-                                        <!-- Task Name -->
-                                        <td class="table-text">
-                                            <div>{{ $task->name }}</div>
-                                        </td>
-
-                                        <!-- Delete Button -->
-                                        <td>
-                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
-                                                {!! csrf_field() !!}
-                                                {!! method_field('DELETE') !!}
-
-                                                <button>Delete Task</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                                                            <button>Delete Task</button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
-    @endif
 
 @endsection
